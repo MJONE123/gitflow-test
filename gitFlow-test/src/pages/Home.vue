f69bf7f3c5f4108abeda3f1e77b9ce7e12d92fd2
문준일 <answnsdlfwkd@naver.com>
2024-06-10 16:18:47 +0900

    충돌발생

--- gitFlow-test/src/pages/Home.vue
+++ gitFlow-test/src/pages/Home.vue
@@ -2,10 +2,11 @@
   <div>
     <RouterLink to="/">home</RouterLink>
     <RouterView />
-    <h1>Home view</h1>
+    <h1>Leader!</h1>
   </div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped></style>
+cp949
